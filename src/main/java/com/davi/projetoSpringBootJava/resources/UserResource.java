15c7f9c61b63953a08cc8edfc2f54dbf7a59c448
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-07-17 18:48:15 -0300

    User

---
+++ src/main/java/com/davi/projetoSpringBootJava/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.davi.projetoSpringBootJava.resources;
+
+import com.davi.projetoSpringBootJava.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Joao", "Joao@gmail.com", "999999", "1234");
+        return ResponseEntity.ok().body(u);
+    }
+}
