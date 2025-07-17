08c7d7e9bd1d0dad01de67e777b4bb6a4d8aa660
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-07-17 18:28:22 -0300

    primeiro commit

---
+++ src/main/java/com/davi/projetoSpringBootJava/ProjetoSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.davi.projetoSpringBootJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/projetoSpringBootJava/ProjetoSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.projetoSpringBootJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
