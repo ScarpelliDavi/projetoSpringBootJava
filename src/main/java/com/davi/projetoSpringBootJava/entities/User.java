94e10c65e50c5b9871a6a20b1ca45a01615b5710
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-07-21 16:23:43 -0300

    table order

--- src/main/java/com/davi/projetoSpringBootJava/entities/User.java
+++ src/main/java/com/davi/projetoSpringBootJava/entities/User.java
@@ -2,12 +2,17 @@ package com.davi.projetoSpringBootJava.entities;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +21,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -66,6 +74,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
