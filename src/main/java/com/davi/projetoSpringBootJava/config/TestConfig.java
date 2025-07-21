763fbb8dec73a3694a8a491e1563d3a71863bb0a
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-07-21 17:53:30 -0300

    categories

--- src/main/java/com/davi/projetoSpringBootJava/config/TestConfig.java
+++ src/main/java/com/davi/projetoSpringBootJava/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.davi.projetoSpringBootJava.config;
 
+import com.davi.projetoSpringBootJava.entities.Category;
 import com.davi.projetoSpringBootJava.entities.Order;
 import com.davi.projetoSpringBootJava.entities.User;
 import com.davi.projetoSpringBootJava.entities.enums.OrderStatus;
+import com.davi.projetoSpringBootJava.repositories.CategoryRepository;
 import com.davi.projetoSpringBootJava.repositories.OrderRepository;
 import com.davi.projetoSpringBootJava.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -33,7 +38,12 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
     }
 }
