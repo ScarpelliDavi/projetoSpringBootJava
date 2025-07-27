ce4af58683235063061ec128bad087f4da33fff9
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-07-26 23:03:56 -0300

    user update

--- src/main/java/com/davi/projetoSpringBootJava/services/UserService.java
+++ src/main/java/com/davi/projetoSpringBootJava/services/UserService.java
@@ -5,6 +5,7 @@ import com.davi.projetoSpringBootJava.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +30,17 @@ public class UserService {
 
     public void delete(Long id){
         userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity,obj);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
