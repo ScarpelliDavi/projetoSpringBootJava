deb789ee84b169a0d6758f7ff832002549a7feda
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-08-08 16:50:42 -0300

    resourceNotFound exception

--- src/main/java/com/davi/projetoSpringBootJava/services/UserService.java
+++ src/main/java/com/davi/projetoSpringBootJava/services/UserService.java
@@ -2,6 +2,7 @@ package com.davi.projetoSpringBootJava.services;
 
 import com.davi.projetoSpringBootJava.entities.User;
 import com.davi.projetoSpringBootJava.repositories.UserRepository;
+import com.davi.projetoSpringBootJava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findByID(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/com/davi/projetoSpringBootJava/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.davi.projetoSpringBootJava.services.exceptions;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id" + id);
+    }
+}
