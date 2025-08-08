6950290090791dbd180d11a5988e3d65d91ff750
ScarpelliDavi <daviscarpelli8@gmail.com>
2025-08-08 17:03:25 -0300

    database exception

--- src/main/java/com/davi/projetoSpringBootJava/services/UserService.java
+++ src/main/java/com/davi/projetoSpringBootJava/services/UserService.java
@@ -2,8 +2,11 @@ package com.davi.projetoSpringBootJava.services;
 
 import com.davi.projetoSpringBootJava.entities.User;
 import com.davi.projetoSpringBootJava.repositories.UserRepository;
+import com.davi.projetoSpringBootJava.services.exceptions.DatabaseException;
 import com.davi.projetoSpringBootJava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.parser.Entity;
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+           throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
